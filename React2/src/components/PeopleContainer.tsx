8426eeb74ecf2bd99d3a6e031ae8d01fdd636574
Damien Wallis <damie.grobi@gmail.com>
2025-05-16 09:42:14 +0200

    fixed state error

--- React2/src/components/PeopleContainer.tsx
+++ React2/src/components/PeopleContainer.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Card1 from "./Card1";
 
+interface Person {
+  name: string;
+  avatar: string;
+  jobtitle: string;
+}
+
 export default function PeopleContainer() {
-  const [people, setPeople] = useState([]);
+  const [people, setPeople] = useState<Person[]>([]);
 
   useEffect(() => {
     fetch("https://67c08172b9d02a9f224a0b60.mockapi.io/person").then((res) =>
